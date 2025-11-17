717c99dd327dd0a399caec02c02cf0a30d31d560
madela1280 <medela1280@gmail.com>
2025-11-17 17:48:45 +0900

    integrate socket.io into next.js for wss

--- app/components/UnifiedGrid.tsx
+++ app/components/UnifiedGrid.tsx
@@ -14,7 +14,8 @@ function initSocket() {
   if (typeof window === 'undefined') return null;
   if (socket) return socket;
 
-  socket = io("wss://moulab.kr:4001", {
+  socket = io("/", {
+  path: "/socket",
   transports: ["websocket"],
   reconnection: true,
   reconnectionAttempts: 10,
