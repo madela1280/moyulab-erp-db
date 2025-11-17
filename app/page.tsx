37e4089024b167450621e045741a7497d212c49f
madela1280 <medela1280@gmail.com>
2025-11-17 09:32:57 +0900

    update page.tsx build-test-v1

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ import dynamic from 'next/dynamic';
 const Home = dynamic(() => import('./Home'), { ssr: false });
 
 export default function Page() {
-  return <Home />;
+  return (
+    <>
+      <div style={{ color: 'red', fontWeight: 'bold', padding: '4px' }}>
+        build-test-v1
+      </div>
+      <Home />
+    </>
+  );
 }
 
