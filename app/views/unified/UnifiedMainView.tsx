5db6bb5fd6d3732575e6177c5fbadf568eac17b0
madela1280 <medela1280@gmail.com>
2025-11-24 14:14:59 +0900

    Fix import path for UnifiedMainView

--- app/views/unified/UnifiedMainView.tsx
+++ app/views/unified/UnifiedMainView.tsx
@@ -1,21 +1,19 @@
 "use client";
 
-import GridHeader from "@/app/unified/components/GridHeader";
-import GridTable from "@/app/unified/components/GridTable";
+import GridHeader from "@/unified/components/GridHeader";
+import GridTable from "@/unified/components/GridTable";
 
 export default function UnifiedMainView() {
   return (
-    <div className="w-full h-full" style={{ paddingLeft: "0.3cm", paddingRight: "0.3cm" }}>
+    <div className="w-full h-full">
       <div style={{ height: "0.5cm" }} />
-
       <GridHeader />
-
-      <div style={{ height: "0.3cm" }} />
-
+      <div style={{ height: "0.5cm" }} />
       <GridTable />
     </div>
   );
 }
+
 
 
 
