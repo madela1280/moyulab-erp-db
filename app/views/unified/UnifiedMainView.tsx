12b4e8c8a3c9a5fd5db11fed59f1a4f786ebc75a
madela1280 <medela1280@gmail.com>
2025-11-24 10:46:56 +0900

    fix: rename component to UnifiedMainView (remove Korean name)

--- app/views/unified/UnifiedMainView.tsx
+++ app/views/unified/UnifiedMainView.tsx
@@ -3,18 +3,17 @@
 import GridHeader from "@/app/unified/components/GridHeader";
 import GridTable from "@/app/unified/components/GridTable";
 
-export default function UnifiedView() {
+export default function UnifiedMainView() {
   return (
     <div className="w-full h-full">
-      {/* 위 여백 0.5cm */}
       <div style={{ height: "0.5cm" }} />
 
       <GridHeader />
-
-      {/* 아래 여백 */}
       <div style={{ height: "0.5cm" }} />
 
       <GridTable />
     </div>
   );
 }
+
+
