a3ced55def990a75e0b6b35d23ed8f6e45788aa5
madela1280 <medela1280@gmail.com>
2025-11-27 10:12:36 +0900

    fix socket-client import path

--- app/unified/components/UnifiedGrid.tsx
+++ app/unified/components/UnifiedGrid.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState, useRef } from "react";
-import socket from "@/moyulab-socket/socket-client.js";
+import socket from "../../../moyulab-socket/socket-client.js";
 
 type UnifiedRow = { id: number; data: Record<string, any> };
 
