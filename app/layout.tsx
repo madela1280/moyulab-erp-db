ef78fc8179d50c8e9d40b10b507f88a9004ce8f0
madela1280 <medela1280@gmail.com>
2025-12-05 10:46:38 +0900

    Fix root layout flex height so unified grid fills viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-gray-100 min-h-screen`}
       >
-        <div className="w-full flex flex-col">
-          <main className="w-full min-h-screen">
+        {/* 화면 전체를 flex 컨테이너로 만들어서 자식이 정확히 viewport 높이를 기준으로 나뉘도록 */}
+        <div className="w-full min-h-screen flex">
+          <main className="w-full flex-1 flex flex-col min-h-0">
             {children}
           </main>
         </div>
