b1ec66cf7293b8ad47678bc436b2c75f2df6dfaf
madela1280 <medela1280@gmail.com>
2025-12-08 14:49:24 +0900

    Fix UnifiedMainView import (remove UnifiedGridHandle)

--- app/views/unified/UnifiedMainView.tsx
+++ app/views/unified/UnifiedMainView.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import { useRef } from "react";
 import GridHeader from "@/unified/components/GridHeader";
-import UnifiedGrid, {
-  UnifiedGridHandle,
-} from "@/unified/components/UnifiedGrid";
+import UnifiedGrid from "@/unified/components/UnifiedGrid";
 
 export default function UnifiedMainView() {
-  const gridRef = useRef<UnifiedGridHandle | null>(null);
-
   return (
     // 통합관리 전용 레이아웃
     <div className="w-full h-full flex flex-col">
@@ -16,21 +11,15 @@ export default function UnifiedMainView() {
       <div style={{ height: "0.3cm" }} />
 
       {/* 버튼 영역 */}
-      <GridHeader
-        onAdd10={async () => {
-          // 행 10개 추가
-          await gridRef.current?.appendBlankRows(10);
-        }}
-      />
+      <GridHeader onAdd10={() => {}} />
 
       {/* 그리드 */}
       <div className="flex-1 min-h-0">
-        <UnifiedGrid ref={gridRef} />
+        <UnifiedGrid />
       </div>
     </div>
   );
 }
-
 
 
 
