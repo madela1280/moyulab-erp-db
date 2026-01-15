5f824c5d6710d1f78b1bcecee81f88f5cbf0e5ac
madela1280 <medela1280@gmail.com>
2026-01-15 15:26:56 +0900

    signup grid: reduce toolbar-to-grid spacing

--- app/views/dataUpload/components/SignupGrid.tsx
+++ app/views/dataUpload/components/SignupGrid.tsx
@@ -455,7 +455,7 @@ export default function SignupGrid({
 
   return (
     <div
-      className="w-full flex flex-col gap-3 flex-1 min-h-0"
+      className="w-full flex flex-col gap-1.1 flex-1 min-h-0"
       onKeyDownCapture={handleKeyDownCapture}
       onPasteCapture={handlePasteCapture}
     >
